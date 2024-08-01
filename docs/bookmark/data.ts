7fb9e82b812a29f902a409221e0a935ce3e390b6
zhuangzhuang <17610835880@163.com>
2024-08-01 08:59:18 +0800

    修改

--- docs/bookmark/data.ts
+++ docs/bookmark/data.ts
@@ -40,10 +40,237 @@ export const NAV_DATA: NavData[] = [
         link: 'https://juejin.cn/post/7103081918266081287'
       },
       {
-        // icon: './images/issue-38.png',
         title: 'VitePress打造个人导航',
         desc: '使用 VitePress 打造个人前端导航网站',
         link: 'https://notes.fe-mm.com/daily-notes/issue-38'
+      },
+      {
+        icon: 'https://p6-passport.byteacctimg.com/img/user-avatar/c1998c9bd48684eb72fa68d4bc262fc4~60x60.awebp',
+        title: 'Obsidian',
+        desc: '为什么我放弃了有道云笔记，选择了 Obsidian',
+        link: 'https://juejin.cn/post/7220698356775469113'
+      },
+      {
+        icon: '/icons/github.svg',
+        title: 'ProxyPool',
+        desc: 'ProxyPool 代理池',
+        link: 'https://github.com/Python3WebSpider/ProxyPool'
+      },
+      {
+        icon: 'http://g1879.gitee.io/drissionpagedocs/img/color_logo.png',
+        title: 'DrissionPage',
+        desc: 'DrissionPage 网页自动化',
+        link: 'http://g1879.gitee.io/drissionpagedocs/get_start/installation/'
+      },
+      {
+        icon: 'https://p9-passport.byteacctimg.com/img/user-avatar/d2e87217a15e32072eb29144fcd8fce5~60x60.awebp',
+        title: 'Vue3基础知识',
+        desc: '2024光速学习Vue3基础知识',
+        link: 'https://juejin.cn/post/7331301209339166772'
+      },
+      {
+        icon: 'https://p3-passport.byteacctimg.com/img/user-avatar/205489b818c8ecd9ddb3a148dbd11a38~120x120.awebp',
+        title: '微前端五大门派大',
+        desc: '微前端五大门派大',
+        link: 'https://juejin.cn/post/7338230967390224435'
+      },
+      {
+        icon: 'https://p3-passport.byteacctimg.com/img/user-avatar/d5a883014e7572b4909cf839e8932e58~120x120.awebp',
+        title: 'ChatGPT-Next-Web',
+        desc: '56K star！一键拥有跨平台 AI 助手：ChatGPT-Next-Web',
+        link: 'https://juejin.cn/post/7320288262400180261'
+      },
+      {
+        icon: 'https://p3-passport.byteacctimg.com/img/user-avatar/f59ff6440f79f4378b2a70690403d4ff~120x120.awebp',
+        title: '前端，该卷什么呢',
+        desc: '【2024】前端，该卷什么呢？',
+        link: 'https://juejin.cn/post/7329126541320536074'
+      },
+      {
+        icon: 'https://p9-passport.byteacctimg.com/img/user-avatar/bf9ea7f023cfe5c1344b6e55b93f52f1~60x60.awebp',
+        title: 'Vite创建Vue3项目',
+        desc: 'Vite创建Vue3项目(一)——配置Vite4+Vue3+Eslint+Prettier',
+        link: 'https://juejin.cn/post/7197656815923118139'
+      } 
+    ]
+  },
+  {  // {
+      //   icon: 'https://pan666.net/favicon.ico',
+      //   title: '阿里小站',
+      //   desc: '我的小站-阿里云盘资源共享站',
+      //   link: 'https://pan666.net/'
+      // },
+    title: '工具',
+    items: [
+    
+      {
+        icon: 'https://raw.githubusercontent.com/jianglin2020/picgo_img/main/img/202405141533535.webp',
+        title: '提取微信聊天',
+        desc: '提取微信聊天记录的工具',
+        link: 'https://hellogithub.com/repository/93df3704446343068e67fc174a34be47'
+      },
+      {
+        icon: '/icons/github.svg',
+        title: 'Music-Tag-Web',
+        desc: '编辑歌曲文件元数据的 Web 应用',
+        link: 'https://github.com/xhongc/music-tag-web'
+      },
+      {
+        icon: 'https://any86.github.io/any-rule/favicon.ico',
+        title: '正则大全',
+        desc: '正则大全',
+        link: 'https://any86.github.io/any-rule/'
+      },
+      {
+        icon: 'https://www.lang123.top/icon.png',
+        title: '琅琅配音',
+        desc: '琅琅配音',
+        link: 'https://www.lang123.top/task'
+      },
+      {
+        icon: 'https://qread.xmsoushu.com/content/images/size/w256h256/format/jpeg/2022/11/favicon.jpg',
+        title: '熊猫速读',
+        desc: '熊猫速读，为你精心推荐视频书，速读、速览一本书，就来熊猫速读',
+        link: 'https://qread.xmsoushu.com/'
+      },
+      {
+        icon: 'https://p3-passport.byteacctimg.com/img/user-avatar/7c861ba3d4cfd3e73c88b6a3637b6bcf~60x60.awebp',
+        title: '英语学习',
+        desc: '英语学习之路',
+        link: 'https://juejin.cn/post/7333162360360665099'
+      },
+      {
+        icon: 'https://picx.zhimg.com/v2-5e6ef8eec181f25c8a450f648153707c_l.jpg?source=2c26e567',
+        title: '成长的私人暗器',
+        desc: '你成长的私人暗器是什么？',
+        link: 'https://www.zhihu.com/question/537552336'
+      }  
+    ]
+  },
+  {
+    title: '云盘',
+    items: [
+      {
+        icon: '/icons/xiaozhan.ico',
+        title: '阿里小站',
+        desc: '我的小站-阿里云盘资源共享站',
+        link: 'https://myxiaozhan.net'
+      },
+      {
+        icon: 'https://img.qwps.cn/656235362?imageMogr2/thumbnail/180x180!&k=1713947005947521929',
+        title: '资享会',
+        desc: '资享会-分享文档',
+        link: 'https://www.kdocs.cn/l/cpfbm3z2y81I'
+      },
+      // {
+      //   icon: 'https://yiso.fun/static/img/logo.png',
+      //   title: '易搜',
+      //   desc: '易搜-强大的网盘搜索引擎|百度|阿里|夸克',
+      //   link: 'https://yiso.fun/'
+      //   // http://yiso.eu.org
+      // },
+      {
+        icon: 'https://s1.imagehub.cc/images/2023/06/14/f6e0735907bfc4d2be567f9e06d12063.png',
+        title: '玩偶',
+        desc: '玩偶哥哥-阿里云盘资源分享',
+        link: 'https://www.wogg.net/'
+      },
+      {
+        icon: 'https://duopan.fun/upload/dycms/20240530-1/9b409f92d6516bcbcf35f3131cb93ac2.jpg',
+        title: '蜡笔',
+        desc: '蜡笔盘盘资源-阿里云盘,夸克云盘',
+        link: 'https://duopan.fun'
+      },
+      {
+        icon: 'https://tudou.lvdoui.top/template/DYXS2/static/picture/logo.png',
+        title: '土豆',
+        desc: '土豆阿里资源站',
+        link: 'https://tudou.lvdoui.top'
+      }
+    ]
+  },
+  {
+    title: 'AI导航',
+    items: [
+      {
+        icon: 'https://img.alicdn.com/imgextra/i3/O1CN01sffRIx1nb3dXCKdFC_!!6000000005107-2-tps-1024-1024.png',
+        title: '通义千问',
+        desc: '通义千问',
+        link: 'https://tongyi.aliyun.com/qianwen'
+      }
+    ]
+  },
+  {
+    title: 'switch',
+    items: [
+      {
+        icon: 'https://avatars.githubusercontent.com/u/7413161?v=4',
+        title: 'ns-emu-tools',
+        desc: 'ns-emu-tools',
+        link: 'https://github.com/triwinds/ns-emu-tools/releases'
+      },
+      {
+        icon: 'https://git.suyu.dev/assets/img/logo.png',
+        title: 'suyu',
+        desc: 'suyu模拟器',
+        link: 'https://git.suyu.dev/suyu/suyu/releases'
+      }
+    ]
+  },
+  {
+    title: '羊毛',
+    items: [
+      {
+        icon: 'https://avatars.githubusercontent.com/u/82091384?v=4',
+        title: '惜之酱',
+        desc: '惜之酱仓库github',
+        link: 'https://github.com/kxs2018/xiaoym'
+      },
+      {
+        icon: 'https://gitlab.com/uploads/-/system/user/avatar/18406011/avatar.png',
+        title: '惜之酱',
+        desc: '惜之酱仓库gitlab',
+        link: 'https://gitlab.com/xizhiai/xiaoym'
+      },
+      {
+        icon: 'https://avatars.githubusercontent.com/u/103831852?s=48&v=4',
+        title: '撸毛日记',
+        desc: '撸毛日记仓库github',
+        link: 'https://github.com/leafTheFish/DeathNote'
+      },
+      {
+        icon: '/icons/github.svg',
+        title: '洛雪音乐',
+        desc: 'lx-music-desktop',
+        link: 'https://github.com/lyswhut/lx-music-desktop/releases/tag/v2.7.0'
+      }
+    ]
+  },
+  {
+    title: 'tvbox',
+    items: [
+      {
+        icon: 'https://xn--sss604efuw.com/z/favicon.ico',
+        title: '饭太硬',
+        desc: '饭太硬',
+        link: 'https://饭太硬.com'
+      },
+      {
+        icon: '/icons/github.svg',
+        title: '我的电视',
+        desc: '我的电视github',
+        link: 'https://github.com/lizongying/my-tv/releases/'
+      },
+      {
+        icon: 'http://tonkiang.us/favicon.ico',
+        title: '直播源搜索',
+        desc: '电视直播源搜索',
+        link: 'http://tonkiang.us/'
+      },
+      {
+        title: '电视直播源',
+        desc: '电视直播源',
+        link: 'https://www.ahhhhfs.com/?s=%E7%9B%B4%E6%92%AD%E6%BA%90'
       }
     ]
   }
--- docs/crm/data.ts
+++ docs/crm/data.ts
@@ -7,60 +7,47 @@ type NavData = {
 
 export const NAV_DATA: NavData[] = [
   {
-    title: '206',
+    title: '本地',
     items: [
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
-        title: 'crm',
-        desc: 'membertest.icfo.cn',
-        link: 'http://membertest.icfo.cn'
-      },
-      {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
-        title: '数据中台',
-        desc: 'membertest.icfo.cn/bi',
-        link: 'http://membertest.icfo.cn/bi/#/login'
-      },
-      {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
-        title: '微信侧边栏',
-        desc: 'jccrmtest.icfo.cn',
-        link: 'http://jccrmtest.icfo.cn/#/login'
-      },
-      {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
-        title: 'odoo',
-        desc: 'crm-odoo-test.icfo.cn',
-        link: 'http://crm-odoo-test.icfo.cn/#/odoo/customerDetails?id=743340'
-      },
-      {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
-        title: 'app',
-        desc: 'apptest.icfo.cn',
-        link: 'http://apptest.icfo.cn/#/login'
-      },
-      {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
-        title: '客户服务平台',
-        desc: 'appmembertest.icfo.cn',
-        link: 'http://appmembertest.icfo.cn/#/login'
-      },
-      {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
-        title: '金财云crm',
-        desc: 'crm-test.icfo.cn/lhyg',
-        link: ' http://crm-test.icfo.cn/lhyg/#/login'
+        icon: '/icons/crm.ico',
+        title: '企微工作台',
+        desc: '192.168.98.120',
+        link: 'http://192.168.98.120:8888'
       }
     ]
   },
   {
-    title: '185',
+    title: '测试48',
     items: [
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: 'crm',
-        desc: 'crm-test.icfo.cn',
-        link: 'http://crm-test.icfo.cn/#/login'
+        desc: '192.168.98.48',
+        link: 'http://192.168.98.48'
+      },
+      {
+        icon: '/icons/crm.ico',
+        title: '数据中台',
+        desc: '192.168.98.48:8888',
+        link: 'http://192.168.98.48:8888'
+      }
+    ]
+  },
+  {
+    title: '测试117',
+    items: [
+      {
+        icon: '/icons/crm.ico',
+        title: 'crm',
+        desc: '192.168.99.117',
+        link: 'http://192.168.99.117'
+      },
+      {
+        icon: '/icons/crm.ico',
+        title: '数据中台',
+        desc: '192.168.99.117:8888',
+        link: 'http://192.168.99.117:8888'
       }
     ]
   },
@@ -68,19 +55,19 @@ export const NAV_DATA: NavData[] = [
     title: '预发布',
     items: [
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: 'crm',
         desc: 'crm-release.icfo.cn',
         link: 'http://crm-release.icfo.cn'
       },
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: 'app',
         desc: 'app-release.icfo.cn',
         link: 'http://app-release.icfo.cn'
       },
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: '客户服务平台',
         desc: 'member-release.icfo.cn',
         link: 'http://member-release.icfo.cn'
@@ -91,37 +78,37 @@ export const NAV_DATA: NavData[] = [
     title: '线上',
     items: [
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: 'crm',
         desc: 'jccrm.icfo.cn',
         link: 'http://jccrm.icfo.cn'
       },
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: '数据中台',
         desc: 'tdc.icfo.cn',
         link: 'http://tdc.icfo.cn'
       },
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: 'app',
         desc: 'app.icfo.cn',
         link: 'http://app.icfo.cn/#/login'
       },
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: '客户服务平台',
         desc: 'member.icfo.cn',
         link: 'http://member.icfo.cn/#/login'
       },
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: '商战智慧',
         desc: 'crm.shangzhanzhihui.com',
         link: 'http://crm.shangzhanzhihui.com/#/login'
       },
       {
-        icon: 'http://membertest.icfo.cn/static/favicon.ico',
+        icon: '/icons/crm.ico',
         title: '金财财税学院',
         desc: 'hy.icfo.cn',
         link: 'http://hy.icfo.cn/index'
